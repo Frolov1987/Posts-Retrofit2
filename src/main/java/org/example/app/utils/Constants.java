db24da9adc79d5615156c7c121783b0aac9ba0e0
Frolov1987 <116515964+Frolov1987@users.noreply.github.com>
2023-08-30 15:04:39 +0300

    Initial commit

---
+++ src/main/java/org/example/app/utils/Constants.java
@@ -0,0 +1,8 @@
+package org.example.app.utils;
+
+public final class Constants {
+    public static final String BASE_URL = "https://jsonplaceholder.typicode.com/";
+    public final static String APP_CLOSED_MSG = "Closing the application.";
+    public final static String NO_DATA_MSG = "No data available.";
+}
+
